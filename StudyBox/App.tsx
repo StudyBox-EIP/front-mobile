73b07c6023a2691e34008f40050c7dfb36064517
antoine.treboutte <antoine.treboutte@iot.bzh>
2021-09-02 17:12:47 -0400

    Login page: add home page and login
    Add the basic auth page (see mockup on onedrive) and login page view
    Add studybox main font (RopaSans) on Android devices, need check on IOS
    devices. This commit adds only the front view, they are no interactions
    with the back-end
    
    Signed-off-by: antoine.treboutte <antoine.treboutte@iot.bzh>

--- StudyBox/App.tsx
+++ StudyBox/App.tsx
@@ -11,71 +11,44 @@
 import 'react-native-gesture-handler';
 
 import * as React from 'react';
-import {View, StyleSheet, Button, SafeAreaView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
+import {ScreenAuth} from './src/components/connection/screen-auth/screen-auth';
 import {MicrosoftOauthScreen} from './src/components/connection/oauth/MicrosoftOauthScreen';
 import {GoogleOauthScreen} from './src/components/connection/oauth/GoogleOauthScreen';
-import {ManualSignInScreen} from './src/components/connection/login/login';
+import {Login} from './src/components/connection/login/login';
 import {ManualSignUpScreen} from './src/components/connection/register/register';
 import {HomePageScreen} from './src/components/homepage/homepage';
 
-const ConnectionScreenStyle = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    marginHorizontal: 16,
-  },
-  base: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderColor: 'green',
-    borderWidth: 1,
-    borderRadius: 10,
-    marginVertical: 10,
-  },
-});
-
-function ConnectionScreen({navigation}: any) {
-  return (
-    <SafeAreaView style={ConnectionScreenStyle.container}>
-      <View style={ConnectionScreenStyle.base}>
-        <Button
-          title="Manual Sign In"
-          onPress={() => navigation.navigate('ManualSignIn')}
-        />
-        <Button
-          title="Manual Sign Up"
-          onPress={() => navigation.navigate('ManualSignUp')}
-        />
-      </View>
-      <View style={ConnectionScreenStyle.base}>
-        <Button
-          title="GOOGLE"
-          onPress={() => navigation.navigate('GoogleOauthScreen')}
-        />
-      </View>
-      <View style={ConnectionScreenStyle.base}>
-        <Button
-          title="MICROSOFT"
-          onPress={() => navigation.navigate('MicrosoftOauthScreen')}
-        />
-      </View>
-    </SafeAreaView>
-  );
-}
-
 const Stack = createStackNavigator();
+const ScreensOptions = {
+  title: '',
+  headerStyle: {
+    backgroundColor: '#F1F1F1',
+  },
+  headerTintColor: '#29B6F6',
+};
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Connection">
-        <Stack.Screen name="Connection" component={ConnectionScreen} />
-        <Stack.Screen name="ManualSignIn" component={ManualSignInScreen} />
-        <Stack.Screen name="ManualSignUp" component={ManualSignUpScreen} />
+      <Stack.Navigator initialRouteName="ScreenAuth">
+        <Stack.Screen
+          name="AuthScreen"
+          options={{headerShown: false}}
+          component={ScreenAuth}
+        />
+        <Stack.Screen
+          name="ManualSignIn"
+          options={ScreensOptions}
+          component={Login}
+        />
+        <Stack.Screen
+          name="ManualSignUp"
+          options={ScreensOptions}
+          component={ManualSignUpScreen}
+        />
         <Stack.Screen name="GoogleOauthScreen" component={GoogleOauthScreen} />
         <Stack.Screen
           name="MicrosoftOauthScreen"
--- StudyBox/src/components/connection/login/login.tsx
+++ StudyBox/src/components/connection/login/login.tsx
@@ -1,35 +1,95 @@
 import React from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
-const ManualSignInScreenStyle = StyleSheet.create({
+const LoginStyle = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
-    marginHorizontal: 16,
+  },
+  logo: {
+    width: 146,
+    height: 121,
+    borderWidth: 5,
+    alignSelf: 'center',
+    marginTop: '-40%',
+    marginBottom: '20%',
+  },
+  container_input: {
+    width: '70%',
+  },
+  label_text: {
+    fontSize: 16,
+    fontFamily: 'RopaSans-Regular',
+    color: '#737373',
   },
   textInput: {
-    borderColor: 'black',
-    borderRadius: 16,
-    borderWidth: 1,
-    color: 'black',
-    minWidth: '60%',
-    backgroundColor: 'grey',
+    borderRadius: 7,
+    marginBottom: 15,
+    backgroundColor: '#FFF',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+    elevation: 10,
+  },
+  button_login: {
+    width: '60%',
+    borderColor: 'transparent',
+    paddingTop: 12,
+    paddingBottom: 12,
+    borderRadius: 7,
+    marginBottom: 15,
+    backgroundColor: '#9CCC65',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+    elevation: 10,
+    marginTop: '10%',
+  },
+  button_text: {
+    color: '#FFF',
+    textAlign: 'center',
+    fontSize: 17,
+    fontFamily: 'RopaSans-Regular',
+    fontWeight: 'bold',
   },
 });
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function ManualSignInScreen({navigation}: any) {
+export function Login({navigation}: any) {
   return (
-    <View style={ManualSignInScreenStyle.container}>
-      <Text>Enter your Credentials</Text>
-      <TextInput
-        style={ManualSignInScreenStyle.textInput}
-        placeholder="Username"
+    <View style={LoginStyle.container}>
+      <Image
+        style={LoginStyle.logo}
+        source={require('../../../assets/studybox-logo.png')}
       />
-      <TextInput
-        style={ManualSignInScreenStyle.textInput}
-        placeholder="Password"
-      />
+      <View style={LoginStyle.container_input}>
+        <Text style={LoginStyle.label_text}>Username</Text>
+        <TextInput style={LoginStyle.textInput} />
+      </View>
+      <View style={LoginStyle.container_input}>
+        <Text style={LoginStyle.label_text}>Password</Text>
+        <TextInput style={LoginStyle.textInput} secureTextEntry={true} />
+      </View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('')}
+        style={LoginStyle.button_login}>
+        <Text style={LoginStyle.button_text}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
