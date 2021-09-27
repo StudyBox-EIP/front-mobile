eb7ee1015f0238726174e76bf8c7602623cd5e70
Nathan.M <nathanmoignard@gmail.com>
2021-09-27 02:46:45 +0200

    [ADD] Bottom Controller on HomePage Structure

--- StudyBox/src/components/pages/homepage/homepage.tsx
+++ StudyBox/src/components/pages/homepage/homepage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 const HomePageScreenStyle = StyleSheet.create({
@@ -7,26 +8,32 @@ const HomePageScreenStyle = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
+  cardContainer: {
+    width: '100%',
+  },
+  cardContentContainer: {
+    alignItems: 'center',
+  },
 });
 
 const cardStyle = StyleSheet.create({
   container: {
-    width: 300,
+    width: '85%',
     height: 250,
     color: '#737373',
     alignItems: 'center',
-    // justifyContent: 'center',
     backgroundColor: 'grey',
     shadowOffset: {
-      width: 20,
-      height: 20,
+      width: 0,
+      height: 5,
     },
     shadowOpacity: 0.1,
     shadowRadius: 1,
     shadowColor: '#000',
-    elevation: 10,
+    elevation: 2,
     borderTopLeftRadius: 10,
     borderBottomRightRadius: 10,
+    marginVertical: 10,
   },
   imageCover: {
     flex: 0.8,
@@ -34,12 +41,38 @@ const cardStyle = StyleSheet.create({
     alignSelf: 'baseline',
     width: 250,
     height: 200,
+    borderTopLeftRadius: 10,
+    borderBottomRightRadius: 10,
   },
   title: {
     position: 'absolute',
     alignSelf: 'baseline',
-    bottom: 25,
+    bottom: 15,
     left: 25,
+    fontSize: 20,
+  },
+});
+
+const pageBottomMargin = StyleSheet.create({
+  container: {
+    position: 'relative',
+    width: '100%',
+    height: 75,
+    bottom: 0,
+  },
+});
+
+const pageController = StyleSheet.create({
+  container: {
+    paddingHorizontal: '10%',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    position: 'absolute',
+    width: '100%',
+    height: 75,
+    backgroundColor: 'white',
+    bottom: 0,
   },
 });
 
@@ -50,9 +83,8 @@ const Card = (props: any) => {
       {/* <Button title={props.title} onPress={() => {}}></Button> */}
       <Image
         style={cardStyle.imageCover}
-        source={{
-          uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
-        }}
+        resizeMode="cover"
+        source={require('../../../assets/Places/Desktop1.jpg')}
       />
     </SafeAreaView>
   );
@@ -62,7 +94,20 @@ export class HomePageScreen extends React.Component {
   render() {
     return (
       <View style={HomePageScreenStyle.base}>
-        <Card title="1ère vignette" />
+        <ScrollView
+          style={HomePageScreenStyle.cardContainer}
+          contentContainerStyle={HomePageScreenStyle.cardContentContainer}
+          showsVerticalScrollIndicator={false}>
+          <Card title="1ère vignette" imageSource="IMAGE URL" />
+          <Card title="2ème vignette" imageSource="IMAGE URL" />
+          <Card title="3ème vignette" imageSource="IMAGE URL" />
+        </ScrollView>
+        <View style={pageBottomMargin.container} />
+        <View style={pageController.container}>
+          <Text>1</Text>
+          <Text>2</Text>
+          <Text>3</Text>
+        </View>
       </View>
     );
   }
