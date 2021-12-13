56b05634cbf726dbf491c067c88953037ae8fc97
Nathan.M <nathanmoignard@gmail.com>
2021-12-13 01:28:15 +0000

    [ADD] New Settings Button Template (prototype) | Disconnection Done

--- StudyBox/src/components/api/auth.ts
+++ StudyBox/src/components/api/auth.ts
@@ -4,11 +4,10 @@ import {API} from '../../../config';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const storeData = async (key: string, value: string) => {
-  console.log('storing data');
   try {
     await AsyncStorage.setItem(key, value);
   } catch (e) {
-    // saving error
+    console.error(e);
   }
 };
 
--- StudyBox/src/components/pages/settings/settings.tsx
+++ StudyBox/src/components/pages/settings/settings.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Text} from 'react-native-svg';
+import {StyleSheet, Text, View} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {BottomHomePageController} from '../../elements/controllers/homePageController';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+const removeValue = async (key: string): Promise<boolean> => {
+  try {
+    await AsyncStorage.removeItem(key);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
 
 const SettingsPageScreenStyle = StyleSheet.create({
   base: {
@@ -10,11 +20,49 @@ const SettingsPageScreenStyle = StyleSheet.create({
   },
 });
 
+const SettingsButton = (props: any) => {
+  const style = StyleSheet.create({
+    container: {
+      backgroundColor: props.color,
+      width: '80%',
+      height: '10%',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginVertical: 10,
+      borderRadius: 10,
+    },
+  });
+
+  return (
+    <View style={style.container}>
+      <TouchableOpacity onPress={props.callback}>
+        <Text>{props.text}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
 export class SettingsScreen extends React.Component<Props> {
+  async disconnect(navigation: any) {
+    if ((await removeValue('token')) === true) {
+      navigation.navigate('AuthScreen');
+    }
+  }
+
   render() {
     return (
       <View style={SettingsPageScreenStyle.base}>
-        <Text>Settings</Text>
+        <SettingsButton text="Mes Amis" color="grey" callback={undefined} />
+        <SettingsButton
+          text="Mes Groupes de Travail"
+          color="grey"
+          callback={undefined}
+        />
+        <SettingsButton
+          text="Déconnexion"
+          color="red"
+          callback={() => this.disconnect(this.props.navigation)}
+        />
         <BottomHomePageController navigation={this.props.navigation} />
       </View>
     );
