747992c9c9d2760141f94d6e1796b5db8808cc9c
BLARD <justin.blard@epitech.eu>
2022-04-27 06:20:03 +0100

    [UPD] Adding search bar.

--- StudyBox/src/components/pages/homepage/homepage.tsx
+++ StudyBox/src/components/pages/homepage/homepage.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import {BottomHomePageController} from '../../elements/controllers/homePageController';
 import {getPictureObject} from '../../../tools/images';
@@ -15,6 +22,22 @@ const HomePageScreenStyle = StyleSheet.create({
   },
   cardContentContainer: {
     alignItems: 'center',
+  },
+  textInput: {
+    marginTop: 8,
+    marginBottom: 2,
+    width: '92%',
+    borderRadius: 7,
+    backgroundColor: '#FFF',
+    shadowColor: '#000',
+    color: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+    elevation: 8,
   },
 });
 
@@ -32,7 +55,7 @@ const cardStyle = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 1,
     shadowColor: '#000',
-    elevation: 2,
+    elevation: 8,
     borderTopLeftRadius: 10,
     borderBottomRightRadius: 10,
     marginVertical: 10,
@@ -110,25 +133,36 @@ export class HomePageScreen extends React.Component<Props> {
         image: undefined,
       },
     ];
+    let currentRooms: Array<Any> = rooms;
+    let contextFilter: String = '';
 
     return (
       <View style={HomePageScreenStyle.base}>
+        <TextInput
+          style={HomePageScreenStyle.textInput}
+          onEndEditing={v => {
+            contextFilter = v.nativeEvent.text;
+            console.log('Search : ' + contextFilter);
+          }}
+        />
         <ScrollView
           style={HomePageScreenStyle.cardContainer}
           contentContainerStyle={HomePageScreenStyle.cardContentContainer}
           showsVerticalScrollIndicator={false}>
-          {rooms.map((val, key) => {
-            return (
-              <Card
-                key={key}
-                title={val.title}
-                desc={val.desc}
-                adress={val.adress}
-                score={val.score}
-                image={val.image}
-                navigation={this.props.navigation}
-              />
-            );
+          {currentRooms.map((val, key) => {
+            if (val.title.includes(contextFilter)) {
+              return (
+                <Card
+                  key={key}
+                  title={val.title}
+                  desc={val.desc}
+                  adress={val.adress}
+                  score={val.score}
+                  image={val.image}
+                  navigation={this.props.navigation}
+                />
+              );
+            }
           })}
         </ScrollView>
         <BottomHomePageController navigation={this.props.navigation} />
