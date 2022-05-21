952efc278e4b5cf16ee588028d5d621c750880e5
Nathan.M <nathanmoignard@gmail.com>
2022-05-21 05:54:20 +0100

    [ADD] Friend Management Full API Implementation

--- src/components/api/friends.ts
+++ src/components/api/friends.ts
@@ -1,6 +1,4 @@
 import axios from 'axios';
-import {Alert} from 'react-native';
-
 import {API} from '../../../config';
 import {getData} from './userInfo';
 
@@ -11,7 +9,7 @@ export async function getFriendList() {
       throw 'userInfo not found';
     }
     const userInfo = JSON.parse(rawUserInfo);
-    const route = API.WEB_ROOT + '/auth/' + userInfo.id + '/friends';
+    const route = API.WEB_ROOT + '/users/friends?user_id=' + userInfo.id;
     const res = await axios.get(route, {
       headers: {
         Authorization: 'Bearer ' + userInfo.token,
@@ -26,29 +24,49 @@ export async function getFriendList() {
   }
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function addFriend(newFriendId: number) {
+export async function getFriendRequests() {
   try {
     const rawUserInfo = await getData('userInfo');
     if (rawUserInfo === undefined || rawUserInfo === null) {
       throw 'userInfo not found';
     }
-    const rawOldFriendList: Array<any> | null = await getFriendList();
-    const oldFriendList: Array<any> =
-      rawOldFriendList === null ? [] : rawOldFriendList;
-
-    const userInfo = JSON.parse(rawUserInfo); // will be removed once friend add button is implemented
-
-    const route = API.WEB_ROOT + '/auth/' + userInfo.id + '/friends';
-    const newFriendList = oldFriendList
-      .map(item => item.id)
-      .concat(userInfo.id);
-
-    await axios.put(
-      route,
-      {
-        friends: newFriendList,
+    const userInfo = JSON.parse(rawUserInfo);
+    const routeReceived = API.WEB_ROOT + '/users/friends/request/receive';
+    const resReceived = await axios.get(routeReceived, {
+      headers: {
+        Authorization: 'Bearer ' + userInfo.token,
       },
+    });
+
+    const routeSent = `${API.WEB_ROOT}/users/friends/request/send`;
+    const resSent = await axios.get(routeSent, {
+      headers: {
+        Authorization: 'Bearer ' + userInfo.token,
+      },
+    });
+
+    return resReceived.data.concat(resSent.data);
+  } catch (e) {
+    if (axios.isAxiosError(e)) {
+      console.error(e.code, e.message, e.response?.data);
+    }
+    return undefined;
+  }
+}
+
+export async function addFriend(newFriendMail: string) {
+  try {
+    const rawUserInfo = await getData('userInfo');
+    if (rawUserInfo === undefined || rawUserInfo === null) {
+      throw 'userInfo not found';
+    }
+
+    const userInfo = JSON.parse(rawUserInfo);
+    const route = API.WEB_ROOT + '/users/friends/request/' + newFriendMail;
+
+    await axios.post(
+      route,
+      {},
       {
         headers: {
           Authorization: 'Bearer ' + userInfo.token,
@@ -57,10 +75,58 @@ export async function addFriend(newFriendId: number) {
     );
   } catch (e) {
     if (axios.isAxiosError(e)) {
-      if (e.response?.status === 400) {
-        Alert.alert('Friend not found');
-        return;
-      }
+      console.error(e, e.response?.data);
+    }
+  }
+}
+
+export async function answerFriendRequest(requestID: number, answer: Boolean) {
+  try {
+    const rawUserInfo = await getData('userInfo');
+    if (rawUserInfo === undefined || rawUserInfo === null) {
+      throw 'userInfo not found';
+    }
+
+    const userInfo = JSON.parse(rawUserInfo);
+    const route =
+      API.WEB_ROOT +
+      `/users/friends/request/${requestID}/${
+        answer === true ? 'accept' : 'refuse'
+      }`;
+
+    await axios.delete(route, {
+      headers: {
+        Authorization: 'Bearer ' + userInfo.token,
+      },
+    });
+  } catch (e) {
+    if (axios.isAxiosError(e)) {
+      console.error(e, e.response?.data);
+    }
+  }
+}
+
+export async function removeFriend(friendID: number) {
+  try {
+    const rawUserInfo = await getData('userInfo');
+    if (rawUserInfo === undefined || rawUserInfo === null) {
+      throw 'userInfo not found';
+    }
+
+    const userInfo = JSON.parse(rawUserInfo);
+    const route = `${API.WEB_ROOT}/users/friends/remove/${friendID}`;
+
+    await axios.put(
+      route,
+      {},
+      {
+        headers: {
+          Authorization: 'Bearer ' + userInfo.token,
+        },
+      },
+    );
+  } catch (e) {
+    if (axios.isAxiosError(e)) {
       console.error(e, e.response?.data);
     }
   }
