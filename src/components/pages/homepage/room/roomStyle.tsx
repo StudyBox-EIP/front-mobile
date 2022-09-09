d07ce01eaca1f13316e87520e814347c739f16b2
Nathan.M <nathanmoignard@gmail.com>
2022-09-09 02:24:36 +0200

    [ADD] Room Notation System

---
+++ src/components/pages/homepage/room/roomStyle.tsx
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+import {COLORS_STUDYBOX} from '../../../elements/colors';
+
+export const style = StyleSheet.create({
+  notationContainer: {
+    width: '90%',
+    backgroundColor: COLORS_STUDYBOX.GREY,
+    borderRadius: 10,
+  },
+  starContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingTop: 10,
+  },
+  textContainer: {
+    width: '75%',
+    alignSelf: 'center',
+    marginTop: 10,
+  },
+  text: {
+    textAlign: 'center',
+    paddingBottom: 10,
+    color: 'white',
+  },
+});
