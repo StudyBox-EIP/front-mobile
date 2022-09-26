940987cb4c9075cc7d0e6fd750943317ac18b5fa
Laroustine <justin.blard@epitech.eu>
2022-09-26 20:55:31 +0200

    [UPD] Designs are better now for History

---
+++ src/components/elements/infocardprice.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Value} from 'react-native-reanimated';
+// import {COLORS_STUDYBOX} from './colors';
+
+function InfoCardPrice(props: any) {
+  const style = StyleSheet.create({
+    container: {
+      flexDirection: 'column',
+      backgroundColor: 'white',
+      width: '100%',
+      marginVertical: 8,
+      paddingVertical: 8,
+      alignSelf: 'center',
+      alignItems: 'center',
+      shadowOffset: {
+        width: 0,
+        height: 10,
+      },
+      shadowOpacity: 0.9,
+      shadowRadius: 1,
+      elevation: 8,
+    },
+    title: {
+      fontSize: 18,
+      textDecorationLine: 'underline',
+      paddingBottom: 4,
+    },
+    subtitle: {
+      fontSize: 14,
+      textDecorationLine: 'underline',
+    },
+    txt: {
+      fontSize: 12,
+    },
+    info: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    price: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+  });
+
+  const container =
+    props.style == null
+      ? StyleSheet.compose(props.style, style.container)
+      : style.container;
+  return (
+    <View style={container}>
+      <Text style={style.title}>{props.room}</Text>
+      <View style={style.info}>
+        <Text style={style.subtitle}>Réservation:</Text>
+        <Text style={style.txt}>
+          {' '}{props.time} - {props.date}
+        </Text>
+      </View>
+      <View style={style.info}>
+        <Text style={style.subtitle}>Adresse:</Text>
+        <Text style={style.txt}> {props.adresse}</Text>
+      </View>
+      <View style={style.price}>
+        <Text style={style.subtitle}>Prix:</Text>
+        <Text style={style.txt}> {props.price}€</Text>
+      </View>
+    </View>
+  );
+}
+
+export default InfoCardPrice;
--- src/components/pages/settings/bookingHistory/style.tsx
+++ src/components/pages/settings/bookingHistory/style.tsx
@@ -2,7 +2,10 @@ import {StyleSheet} from 'react-native';
 
 export const historyStyle = StyleSheet.create({
   history_container: {
+    paddingBottom: 2,
     width: '80%',
-    height: 100,
+  },
+  view: {
+    height: '92%',
   },
 });
