c969f4ba2b7daa3b14104e7a39902d0cfd4a80fa
Nathan.M <nathanmoignard@gmail.com>
2022-10-28 16:34:00 +0200

    [ADD] Login begin page

--- src/components/pages/connection/login/login.tsx
+++ src/components/pages/connection/login/login.tsx
@@ -5,6 +5,8 @@ import StudyBoxSubmitButton from '../../../elements/form/submit/StudyBoxSubmitBu
 import {login} from '../../../api/auth';
 import {COLORS_STUDYBOX} from '../../../elements/colors';
 
+import STUDYBOX_ICON from '../../../../assets/svg/logo_studybox_couleur.svg';
+
 const LoginStyle = StyleSheet.create({
   container: {
     flex: 1,
@@ -44,34 +46,70 @@ export function LoginScreen({navigation}: any) {
 
   return (
     <View style={LoginStyle.container}>
-      <Image
-        style={LoginStyle.logo}
-        source={require('../../../../assets/studybox-logo.png')}
-      />
-      <View style={LoginStyle.main_wrapper}>
-        <StudyBoxTextInput
-          text="Email"
-          keyboardType={'email-address'}
-          changeText={changeUsername}
-        />
-        <StudyBoxTextInput
-          text="Mot de Passe"
-          autoCompleteType="password"
-          secureTextEntry={true}
-          changeText={changePassword}
-        />
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('PasswordRecoveryScreen');
-          }}>
-          <Text style={LoginStyle.forgotPassword}>Mot de passe oublié ?</Text>
-        </TouchableOpacity>
-      </View>
-      {/* Backend Authentication */}
-      <StudyBoxSubmitButton
-        text="Se connecter"
-        onPress={() => login(navigation, data)}
-      />
+      {/* Header */}
+      <ConnectionHeader />
+
+      {/* Body */}
+      {/* Title */}
+      {/* Mail */}
+      {/* Passwords */}
+      {/* Forgotten Password */}
+      {/* Connection Button */}
+
+      {/* Footer */}
     </View>
   );
 }
+
+const ConnectionHeader = () => {
+  const style = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    text: {
+      marginLeft: 10,
+    },
+  });
+
+  return (
+    <View style={style.container}>
+      <STUDYBOX_ICON
+        color={COLORS_STUDYBOX.DARK_WHITE}
+        width={50}
+        height={50}
+      />
+      <Text style={style.text}>StudyBox</Text>
+    </View>
+  );
+};
+
+{/* <Image
+  style={LoginStyle.logo}
+  source={require('../../../../assets/studybox-logo.png')}
+/>
+<View style={LoginStyle.main_wrapper}>
+  <StudyBoxTextInput
+    text="Email"
+    keyboardType={'email-address'}
+    changeText={changeUsername}
+  />
+  <StudyBoxTextInput
+    text="Mot de Passe"
+    autoCompleteType="password"
+    secureTextEntry={true}
+    changeText={changePassword}
+  />
+  <TouchableOpacity
+    onPress={() => {
+      navigation.navigate('PasswordRecoveryScreen');
+    }}>
+    <Text style={LoginStyle.forgotPassword}>Mot de passe oublié ?</Text>
+  </TouchableOpacity>
+</View> */}
+{/* Backend Authentication */}
+{/* <StudyBoxSubmitButton
+  text="Se connecter"
+  onPress={() => login(navigation, data)}
+/> */}
