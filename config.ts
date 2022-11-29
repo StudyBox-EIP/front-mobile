78d4206537fbae8cf9237dbf60d419432d72cb85
Nathan.M <nathanmoignard@gmail.com>
2022-11-29 17:23:49 +0100

    [UPDATE] Dynamic Profile Image with background

--- config.ts
+++ config.ts
@@ -2,6 +2,8 @@ import {API_URL} from '@env';
 
 export const API = {
   WEB_ROOT: API_URL,
+  MAPBOX_TOKEN:
+    'pk.eyJ1IjoibmF0aGFuMzMyOSIsImEiOiJjazA2ZXhoNWQwNDIwM2Nscnl3OWtxbzh1In0.soHJHIPW_4ki0G8rf5rNrw',
   STRIPE_PUBLIC_KEY:
     'pk_test_51IcCYYB7CVKNCpls4Bt29ZG7hpmF2QfttGbEWy0CafC8Bvrg1VzhZmgO2vsbLCVjcVgWU3B7FBEsb5qOknJgoaxI00adPvbA8z',
 };
